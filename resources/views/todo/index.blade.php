f81e8740fdacfad9df40f454ad676bab4ebc4412
morifumiya <morimorimorifu@gmail.com>
2019-12-23 08:17:30 +0000

    index.blade.phpのh1をToDo一覧から{{ Auth::user()->name }}のToDo一覧に変更

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,7 +1,7 @@
 @extends ('layouts.app')<!-- resources/views -->
 @section ('content')<!-- contentファイルを読み込み表示 -->
 
-<h1 class="page-header">ToDo一覧</h1>
+<h1 class="page-header">{{ Auth::user()->name }}のToDo一覧</h1>
 <p class="text-right">
   <a class="btn btn-success" href="/todo/create">新規作成</a>
 </p>
